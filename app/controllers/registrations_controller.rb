fc3f154c24adafad4024ac27868d4a26dd6410d6
xkyfal <diogo6204@gmail.com>
2025-02-19 16:04:48 +0000

    users can now register through sign up

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,22 @@
+class RegistrationsController < ApplicationController
+  allow_unauthenticated_access only: [ :new, :create ]
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      start_new_session_for @user
+      redirect_to root_path
+    else
+      redirect_to new_registration_path, alert: "Invalid credentials."
+    end
+  end
+
+  private
+    def user_params
+      params.permit(:email_address, :username, :password, :password_confirmation)
+    end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   has_many :sessions, dependent: :destroy
 
   validates_presence_of :username
+  validates :password, confirmation: { case_sensitive: true }
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 end
